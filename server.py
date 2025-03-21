8afc9fca7ad45782f3390e42e89b1ab6b41d76c8
quydang <81231812+quydang04@users.noreply.github.com>
2025-03-21 14:49:59 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,63 +1,39 @@
-from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
+import random
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
 
-import socket
-import threading
-import hashlib
+class WebSocketServer(tornado.websocket.WebSocketHandler):
+    clients = set()
+    def open(self):
+        WebSocketServer.clients.add(self)
+    def on_close(self):
+        WebSocketServer.clients.remove(self)
+    @classmethod
+    def send_message(cls, message: str):
+        print(f"Sending message {message} to {len(cls.clients)} client(s).")
+        for client in cls.clients:
+            client.write_message(message)
+class RandomWordSelector:
+    def __init__(self, word_list):
+        self.word_list = word_list
+    def sample(seft):
+        return random.choice(seft.word_list)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 12345))
-server_socket.listen(5)
-
-server_key = RSA.generate(2048)
-
-clients = []
-
-def encrypt_message(key, message):
-    cipher = AES.new(key, AES.MODE_CBC)
-    ciphertext = cipher.encrypt(pad(message.encode(), AES.block_size))
-    return cipher.iv + ciphertext
-
-def decrypt_message(key, encrypted_message):
-    iv = encrypted_message[:AES.block_size]
-    ciphertext = encrypted_message[AES.block_size:]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    decrypted_message = unpad(cipher.decrypt(ciphertext), AES.block_size)
-    return decrypted_message.decode()
-
-def handle_client(client_socket, client_address):
-    print(f"Connected with {client_address}")
+def main():
+    app = tornado.web.Application(
+        [(r"/websocket/", WebSocketServer)],
+        websocket_ping_interval=10,
+        websocket_ping_timeout=30,
+    )
+    app.listen(8888)
+    io_loop = tornado.ioloop.IOLoop.current()
+    word_selector = RandomWordSelector(['apple', 'banana','orange', 'grape', 'melon'])
+    periodic_callback = tornado.ioloop.PeriodicCallback(
+        lambda:WebSocketServer.send_message(word_selector.sample()), 3000
+    )
+    periodic_callback.start()
+    io_loop.start()
     
-    client_socket.send(server_key.publickey().export_key(format='PEM'))
-    client_received_key = RSA.import_key(client_socket.recv(2048))
-    
-    aes_key = get_random_bytes(16)
-    
-    cipher_rsa = PKCS1_OAEP.new(client_received_key)
-    encrypted_aes_key = cipher_rsa.encrypt(aes_key)
-    client_socket.send(encrypted_aes_key)
-    clients.append((client_socket, aes_key))
-    
-    while True:
-        encrypted_message = client_socket.recv(1024)
-        decrypted_message = decrypt_message(aes_key, encrypted_message)
-        print(f"Received from {client_address}: {decrypted_message}")
-        
-        for client, key in clients:
-            if client != client_socket:
-                encrypted = encrypt_message(key, decrypted_message)
-                client.send(encrypted)
-        
-        if decrypted_message == "exit":
-            break
-    clients.remove((client_socket, aes_key))
-    client_socket.close()
-    print(f"Connection with {client_address} closed")
-    
-while True: 
-    client_socket, client_address = server_socket.accept()
-    client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-    client_thread.start()
-    
+if __name__ == "__main__":
+    main()   
